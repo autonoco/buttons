d8aa4ee27fb89f154a094f9e83e51d5087e47291
Bobak Emamian <bobak.emamian@gmail.com>
2026-04-19 20:27:14 -0400

    chore: drop dev artifacts (.buttons/, buttons-dev) from the repo

--- .buttons/buttons/slow/main.sh
+++
@@ -1,8 +0,0 @@
-
-    for i in 1 2 3 4 5; do
-      echo "step $i of 5"
-      echo "{\"ts\":\"$(date)\",\"event\":\"progress\",\"pct\":$(echo "$i / 5" | bc -l)}" >> $BUTTONS_PROGRESS_PATH
-      sleep 1
-    done
-    echo "{\"done\":true}"
-  
